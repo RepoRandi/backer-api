143ee001946bb2f2dac0361cd7175d84f4feed78
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-11-05 11:03:39 +0700

    Fetch user endpoint

--- handler/user.go
+++ handler/user.go
@@ -177,3 +177,13 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 	return
 }
+
+func (h *userHandler) FetchUser(c *gin.Context) {
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	formatter := user.FormatUser(currentUser, "")
+
+	response := helper.APIResponse("Successfuly fetch user data", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
