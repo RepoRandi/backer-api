b58f319eca70d23cef38ca48ad44eb611d63acc2
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-11-14 16:14:13 +0700

    Get transaction campaign endpoint (handler)

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,40 @@
 package handler
+
+import (
+	"backer-api/helper"
+	"backer-api/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBind(&input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionsByCampaignID(input)
+
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to get campaign's transactions", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
