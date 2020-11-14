608ca7d6eb9f8da20d787311b203f0ea21a1efd9
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-11-14 16:13:11 +0700

    Get transaction campaign endpoint (service)

--- transaction/input.go
+++ transaction/input.go
@@ -1,1 +1,5 @@
 package transaction
+
+type GetCampaignTransactionsInput struct {
+	ID int `uri:"id" binding:"required"`
+}
--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,23 @@
 package transaction
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	transactions, err := s.repository.GetByCampaignID(input.ID)
+
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
