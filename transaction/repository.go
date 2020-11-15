97cb6468cef08e00c1bdc75c818c7791fef2ff3d
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-11-15 20:43:17 +0700

    Get transactions user endpoint (repository)

--- transaction/repository.go
+++ transaction/repository.go
@@ -30,7 +30,7 @@ func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
 func (r *repository) GetByUserID(userID int) ([]Transaction, error) {
 	var transactions []Transaction
 
-	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Find(&transactions).Error
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Order("id desc").Find(&transactions).Error
 
 	if err != nil {
 		return transactions, err
